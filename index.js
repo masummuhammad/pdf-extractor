e32ddcdb023bd91c401ee4dd5c22d70b9d6d14dd
Muhammad Masum <48872682+masummuhammad@users.noreply.github.com>
2021-06-22 15:50:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 const extractor=require('./extractor');
-const fs=require('fs');
-const path=require('path');
-fs.readdir('pdfs',(err,files)=>{
-    if(err)throw err;
-    files.forEach(file=>{
-        extractor(path.join('pdfs',file)).then(e=>{console.log(e);}).catch((err)=>{throw err});
-    })
-})
+extractor(process.argv[2]).then(e=>{console.log(e);}).catch((err)=>{throw err});
+ 
